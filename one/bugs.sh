f5191ab5a721894deb80890d08f922447bcc5e12
ayush-oct0 <ayushsharma.oct0@gmail.com>
2025-07-12 14:37:02 +0530

    Add Bug Files

---
+++ one/bugs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+############################
+
+# Date: 08-07-2025
+# Author: Ayush Sharma
+# Description: A Script File For Test Some Commits
+
+############################
+
+echo "Hello From Local Computer"
+
+
+echo "No Bug Is Found"
---
+++ one/new-bugs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#############################
+# Date: 10-07-2025
+# Author: Ayush Sharma
+# Description: File Of New Bug Fixes
+#############################
+
+echo "This New Bugs Fixes Files"
+
+echo "New Bugs Is Fix"
+
+echo "Bug Is Found In Calling Feature"
+<<<<<<< HEAD
+
+
+echo "Calling Bug Is Fixed"
+=======
+>>>>>>> 784d425 (Bug Found In Calling Feature)
